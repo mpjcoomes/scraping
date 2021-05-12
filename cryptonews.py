3702fd7784d5b85f9bf7cf6d2e1cc6d6f5954275
mpjcoomes <mpjcoomes@gmail.com>
2021-05-12 11:58:35 +1000

    Update cryptonews.py

--- cryptonews.py
+++ cryptonews.py
@@ -22,13 +22,21 @@ df["Market cap"] = df.apply(lambda row: round(float(re.sub('[$,]', '', row["Mark
 df = df[[1, 0, "Market cap"]]
 df.columns = ["CODE", "NAME", "CAP($M)"]
 
-# AU Coinbase supported, 57 as of 07-05-2021, Celo is CGLD on Coinbase, CELO elsewhere.
+# AU Coinbase supported, 58 as of 07-05-2021, Celo is CGLD on Coinbase, CELO elsewhere.
 coinbaseAU = ["1INCH", "AAVE", "ADA", "ALGO", "ANKR", "ATOM", "BAL", "BAND", "BAT", "BCH", "BNT", "BTC", "CELO", "COMP",
-              "CRV", "CTSI", "CVC", "DAI", "DASH", "DNT", "ENJ", "EOS", "ETC", "ETH", "FIL", "FORTH", "GRT", "KNC",
-              "LINK", "LRC", "LTC", "MANA", "MATIC", "MIR", "MKR", "NKN", "NMR", "NU", "OGN", "OMG", "OXT", "REN",
-              "REP", "RLC", "SKL", "SNX", "STORJ", "SUSHI", "TRB", "UMA", "UNI", "WBTC", "XLM", "XTZ", "YFI", "ZEC",
-              "ZRX"]
+              "CRV", "CTSI", "CVC", "DAI", "DASH", "DNT", "ENJ", "EOS", "ETC", "ETH", "FIL", "FORTH", "GRT", "ICP",
+              "KNC", "LINK", "LRC", "LTC", "MANA", "MATIC", "MIR", "MKR", "NKN", "NMR", "NU", "OGN", "OMG", "OXT",
+              "REN", "REP", "RLC", "SKL", "SNX", "STORJ", "SUSHI", "TRB", "UMA", "UNI", "WBTC", "XLM", "XTZ", "YFI",
+              "ZEC", "ZRX"]
 
 df2 = df.loc[df["CODE"].isin(coinbaseAU)]
 df2.reset_index(drop=True, inplace=True)
-df2.to_csv('file4.csv', sep='\t', index=False)
+
+# Read in and join your own buy transactions, supports 1-to-1 only.
+df3 = pd.read_csv("Coinbase.csv", skiprows=7)
+df3.columns = ["DATE_TIME", "TYPE", "CODE", "QUANTITY", "SPOT($)", "COIN($)", "TOTAL($)", "FEE($)", "NOTES"]
+
+df4 = df2.merge(df3, on="CODE", how='left')
+df4 = df4.sort_values(by="CAP($M)", ascending=False)
+
+df4.to_csv('file4.csv', sep='\t', index=False)
