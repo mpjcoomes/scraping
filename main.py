5b31b82bb2f49b2d7c29a484cb55f0712b2bdd20
mpjcoomes <mpjcoomes@gmail.com>
2021-05-22 22:13:19 +1000

    Update main.py

--- main.py
+++ main.py
@@ -7,58 +7,76 @@ from selenium import webdriver
 import re
 import asx_table
 
+
+def skin(patt: str, soup_obj) -> int:
+    match_obj = re.search(patt, soup_obj)
+    if match_obj is not None:
+        sub = re.sub(" +", " ", match_obj.group())
+        sub = re.split(" ", re.sub("[$,]", "", sub))[-1]
+        return sub
+
+
 df_mi = pd.DataFrame()
 
 for i in asx_table.df["CODE"]:
+    # Market Index
     driver = webdriver.Firefox(executable_path="/home/user/geckodriver")
     driver.maximize_window()
-    time.sleep(5)
     driver.get("https://www.marketindex.com.au/asx/" + i)
     time.sleep(5)
     html = driver.page_source
     driver.close()
     soup = BeautifulSoup(html, "html.parser")
     flat = re.sub("[\n]", " ", soup.text)
-    div_yield = re.split(" ", re.search("DIV YIELD [0-9.]*", flat).group())[2]
-    one_yr = re.split(" ", re.search("1 YR RETURN -*[0-9.]*", flat).group())[3]
-    cap = re.split(
-        " ",
-        re.sub("[$,]", "", re.search("Market Capitalisation \$[0-9,]*", flat).group()),
-    )[2]
-    cap = round(float(cap) / 10 ** 6)
+    div_yield = skin("DIV YIELD [0-9.]+", flat)
+    one_yr = skin("1 YR RETURN -*[0-9.]+", flat)
+    cap = round(float(skin("Capitalisation \$[0-9,]+", flat)) / 10 ** 6)
+    # ASX
     driver = webdriver.Firefox(executable_path="/home/user/geckodriver")
     driver.maximize_window()
-    time.sleep(5)
     driver.get("https://www2.asx.com.au/markets/etp/" + i)
     time.sleep(5)
     html = driver.page_source
     driver.close()
     soup = BeautifulSoup(html, "html.parser")
-    frank = re.split(" ", re.search("Franking [0-9]*", soup.text).group())[1]
-    companies = re.split(
-        " ",
-        re.sub("[,]", "", re.search("Total fund holdings [0-9,]*", soup.text).group()),
-    )[3]
-    us = re.search("United States\s{2}[0-9.]*", soup.text)
-    if us is not None:
-        us = re.split(" ", us.group())[3]
-    chyna = re.search("China\s{2}[0-9.]*", soup.text)
-    if chyna is not None:
-        chyna = re.split(" ", chyna.group())[2]
-    hk = re.search("Hong Kong\s{2}[0-9.]*", soup.text)
-    if hk is not None:
-        hk = re.split(" ", hk.group())[3]
+    frank = skin("Franking [0-9]+", soup.text)
+    holdings = skin("Total fund holdings [0-9,]+", soup.text)
+    us = skin("States\s{2}[0-9.]+", soup.text)
+    chyna = skin("China\s{2}[0-9.]+", soup.text)
+    hk = skin("Kong\s{2}[0-9.]+", soup.text)
+    ind = skin("Industrials [0-9.]+", soup.text)
+    mat = skin("Materials [0-9.]+", soup.text)
+    ccy = skin("Consumer Cyclical [0-9.]+", soup.text)
+    tec = skin("Technology [0-9.]+", soup.text)
+    utl = skin("Utilities [0-9.]+", soup.text)
+    cdf = skin("Consumer Defensive [0-9.]+", soup.text)
+    egy = skin("Energy [0-9.]+", soup.text)
+    fin = skin("Financial Services [0-9.]+", soup.text)
+    hlc = skin("Healthcare [0-9.]+", soup.text)
+    com = skin("Communication Services [0-9.]+", soup.text)
+    rel = skin("Real Estate [0-9.]+", soup.text)
     df2 = pd.DataFrame(
         {
             "CODE": i,
             "FRANK(%)": [frank],
-            "HOLDS": [companies],
+            "HOLDS": [holdings],
             "US(%)": [us],
             "CH(%)": [chyna],
             "HK(%)": [hk],
             "CAP($M)": [cap],
-            "YIELD(%)": [div_yield],
+            "YLD(%)": [div_yield],
             "1YR(%)": [one_yr],
+            "CCY(%)": [ccy],
+            "CDF(%)": [cdf],
+            "COM(%)": [com],
+            "EGY(%)": [egy],
+            "FIN(%)": [fin],
+            "HLC(%)": [hlc],
+            "IND(%)": [ind],
+            "MAT(%)": [mat],
+            "REL(%)": [rel],
+            "TEC(%)": [tec],
+            "UTL(%)": [utl],
         }
     )
     df_mi = pd.concat([df_mi, df2])
